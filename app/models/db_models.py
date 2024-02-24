cfb57294f9f7f96c2bca8cf8528bb15ab55ddc06
djjohnsongeek <danieleejohnson@gmail.com>
2024-02-24 12:40:28 -0500

    feat: more model updates

--- app/models/db_models.py
+++ app/models/db_models.py
@@ -2,24 +2,38 @@ import os
 from peewee import *
 from app.models.base import Person, BaseModel
 
+#todo add table name meta
+
 class Student(Person):
     student_number = CharField(max_length=32, unique=True)
     application_date = DateField()
     occupation = CharField(max_length=128)
 
+    class Meta:
+        table_name = "students"
+
 class Staff(Person):
     hashed_password = CharField(max_length=256)
-    pass
 
 class Term(BaseModel):
     name = CharField(max_length=64)
+
+    class Meta:
+        table_name = "terms"
 
 class SchoolClass(BaseModel):
     name = CharField(max_length=64)
     teacher = ForeignKeyField(Staff, backref="classes")
     room_number = IntegerField()
 
-class ClassRosterItem(BaseModel):
+    class Meta:
+        table_name = "classes"
+
+class ClassRosterEntry(BaseModel):
     student = ForeignKeyField(Student, backref="class_list")
     school_class = ForeignKeyField(SchoolClass, backref="student_list")
+
+    class Meta:
+        table_name = "class_rosters"
+
     
