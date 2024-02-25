c1fb73714cb9560516e0b9cd204033858a087b80
djjohnsongeek <danieleejohnson@gmail.com>
2024-02-25 14:50:33 -0500

    feat: adjust enums

--- app/models/enum.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class StaffRole(Enum):
-    TEACHER = 1
-    GENERAL = 2
---
+++ app/models/enums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class StaffRole(Enum):
+    Teacher = 1
+    General = 2
+
+class PersonGender(Enum):
+    Male = 1,
+    Female = 2
