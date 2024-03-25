138f0cba82755b5bdd5a7f27ccdca0c9d8fef870
Daniel <danieleejohnson@gmail.com>
2024-03-25 13:14:52 -0400

    feat: flashes messages on success and error

--- app/controllers/staff_controller.py
+++ app/controllers/staff_controller.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from app.services import staff_service
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from app.services import staff_service, controller_service
 from app.models.forms import StaffEditForm
+from app.models.enums import MessageCategory
 staff_blueprint = Blueprint("staff", __name__)
 
 @staff_blueprint.route("/staff", methods=["GET"])
@@ -23,4 +24,9 @@ def edit(staff_id: int):
         if staff_edit_model is None:
             return redirect(url_for("index.error", error_code=404))
         else:
+
+            if staff_edit_model.form.errors:
+                controller_service.flash_message("Failed to update staff member details.", MessageCategory.Error)
+            else:
+                controller_service.flash_message("Staff member details updated!", MessageCategory.Success)
             return render_template("/staff/edit.html", staff_model=staff_edit_model)
