02cb507f4d13c995fbd5458b8e2eaa4ac692dfdf
Daniel <danieleejohnson@gmail.com>
2024-03-25 13:15:23 -0400

    feat: add update method for Staff model

--- app/repo/staff_repo.py
+++ app/repo/staff_repo.py
@@ -7,11 +7,19 @@ def retrieve_all() -> []:
 def retrieve(staff_id: int) -> Staff:
     return Staff.get_or_none(Staff.id == staff_id)
 
-
-def update(form: StaffEditForm) -> bool:
-
-    return True
-
-    # return Staff.update(
-
-    # )
+def update(form: StaffEditForm, staff: Staff) -> bool:
+    staff.first_name = form.first_name.data
+    staff.first_name_lao = form.first_name_lao.data
+    staff.last_name = form.last_name.data
+    staff.last_name_lao = form.last_name_lao.data
+    staff.username = form.username.data
+    staff.email = form.email.data
+    staff.nick_name = form.nick_name.data
+    staff.phone_number = form.phone_number.data
+    staff.address = form.address.data
+    staff.birthday = form.birthday.data
+    staff.gender = form.gender.data
+    staff.role = form.role.data
+    
+    rows_updated = staff.save()
+    return rows_updated == 1
