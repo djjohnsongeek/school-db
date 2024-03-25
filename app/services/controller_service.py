e110ccda8db169348e9bece52135977f4b8a4cf7
Daniel <danieleejohnson@gmail.com>
2024-03-25 13:14:05 -0400

    feat: add method to flash different kinds of messages

---
+++ app/services/controller_service.py
@@ -0,0 +1,16 @@
+from flask import flash
+from app.models.enums import MessageCategory
+
+def flash_message(message: str, category: MessageCategory):
+    flash_cat = ""
+
+    if category is MessageCategory.Success:
+        flash_cat = "is-success"
+    elif category is MessageCategory.Warn:
+        flash_cat = "is-warning"
+    elif category is MessageCategory.Error:
+        flash_cat = "is-danger"
+    else:
+        flash_cat = ""
+
+    flash(message, flash_cat)
