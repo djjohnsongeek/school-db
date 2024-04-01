e9be148d06fe73c3ed201c718164546e7ee4e535
Daniel <danieleejohnson@gmail.com>
2024-04-01 13:18:58 -0400

    feat: add modal js logic

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -9,3 +9,57 @@ for (let element of document.getElementsByClassName("delete"))
         container.removeChild(notification)
     });
 }
+
+// MODAL LOGIC
+var Modal = {
+    initialize: function()
+    {
+        // trigger open logic
+        for (let element of document.getElementsByClassName("modal-trigger"))
+        {
+            element.addEventListener("click", function(event) {
+                const targetId = event.currentTarget.dataset.target;
+                const itemId = event.currentTarget.dataset.itemId;
+                const targetElement = document.getElementById(targetId);
+                targetElement.dataset.itemId = itemId;
+                Modal.open(targetElement);
+            });
+        }
+        
+        // escape keyboard shortcut
+        document.addEventListener('keydown', function (event) {
+            if(event.key === "Escape") {
+                Modal.closeAll();
+            }
+        });
+
+        // cancel button logic
+        for (let element of document.getElementsByClassName("close-modal"))
+        {
+            element.addEventListener("click", function(event) {
+                const modalElement = event.currentTarget.closest(".modal")
+                if (modalElement)
+                {
+                    Modal.close(modalElement);
+                }
+            });
+        }
+    },
+    open: function(element)
+    {
+        element.classList.add("is-active");
+    },
+    close: function(element)
+    {
+        element.classList.remove("is-active");
+        delete element.dataset.itemId;
+    },
+    closeAll: function()
+    {
+        for (let modalElement of document.getElementsByClassName("modal"))
+        {
+            Modal.close(modalElement);
+        }
+    },
+}
+Modal.initialize();
