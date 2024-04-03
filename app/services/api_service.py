5277564a5eddecb81d1fe5ed23dd2791d25174d7
Daniel <danieleejohnson@gmail.com>
2024-04-03 12:20:56 -0400

    feat: add a todo, use the new jsResponseItem class

--- app/services/api_service.py
+++ app/services/api_service.py
@@ -1,8 +1,10 @@
 from flask import Request, current_app
 from app.services import staff_service
+from app.models.view_models import AsyncJsResponseItem
 
-def delete_item(category: str, action: str, request: Request) -> []:
+def delete_item(category: str, action: str, request: Request) -> AsyncJsResponseItem:
     errors = []
+    # TODO: figure out how to properly parse json
     request_data = request.get_json()
     if request_data:
         item_id = request_data.get("item_id", None)
@@ -22,7 +24,7 @@ def delete_item(category: str, action: str, request: Request) -> []:
             elif category == "student":
                 pass
 
-    return errors
+    return AsyncJsResponseItem(error=error, data={"itemId" : item_id})
 
 def valid_category(category: str) -> bool:
     return category.lower() in current_app.config["ALLOWED_CATEGORIES"]
