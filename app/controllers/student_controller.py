db3c554d1816bbacc9cda01cac2e33d166e22ffb
djjohnsongeek <danieleejohnson@gmail.com>
2024-04-08 06:19:24 -0400

    feat: adds student edit and create routes

--- app/controllers/student_controller.py
+++ app/controllers/student_controller.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from app.services import student_service
+from app.services import student_service, controller_service
+from app.models.view_models import StudentCreateItem
+from app.models.forms import StudentEditForm
+from app.models.enums import MessageCategory
 
 student_blueprint = Blueprint("students", __name__)
 
@@ -17,5 +20,23 @@ def edit(student_id: int):
             return redirect(url_for("index.error", error_code=404))
         else:
             return render_template("/students/edit.html", student_model=student_edit_model)
+    elif request.method == "POST":
+        student_form = StudentEditForm()
+        student_edit_model = student_service.update_student(student_form)
+        if student_edit_model is None:
+            return redirect(url_for("index.error", error_code=404))
+        else:
+            if student_edit_model.form.errors:
+                controller_service.flash_message("Failed to update student's details.", MessageCategory.Error)
+            else:
+                controller_service.flash_message("Student's details updated!", MessageCategory.Success)
+            return render_template("/students/edit.html", student_model=student_edit_model)
+
+@student_blueprint.route("/students/create", methods=["GET", "POST"])
+def create():
+    student_form = StudentEditForm()
+    if request.method == "GET":
+        create_model = StudentCreateItem(student_form)
+        return render_template("/students/create.html", student_model=create_model)
     else:
         pass
