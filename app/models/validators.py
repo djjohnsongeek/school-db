8babd1a71e1996e5f5727926259f4ad5d6e97add
djjohnsongeek <danieleejohnson@gmail.com>
2024-04-11 06:21:22 -0400

    feat: add custom validator for student numbers

--- app/models/validators.py
+++ app/models/validators.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from wtforms import ValidationError
 
 class StudentNumber(object):
     def __init__(self, message=None):
@@ -15,16 +15,12 @@ class StudentNumber(object):
             raise ValidateError
 
         year = int(student_number[-4:])
-        month = int(student_number[-6:][:3])
+        month = int(student_number[-6:][:2])
 
-
-        valid = student_number.isdigit()
-        valid = len(student_number) > 8 and len(student_number) < 33
-        valid = year <= datetime.now().year
-        valid = month > 0 and month < 13
+        valid = (student_number.isdigit() and
+                len(student_number) >= 8 and len(student_number) < 33 and
+                year <= datetime.now().year and year > 2023
+                and month > 0 and month < 13)
 
         if not valid:
-            raise ValidateError
-
-
-
+            raise ValidationError(self.message)
