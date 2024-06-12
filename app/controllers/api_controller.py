69a0371530092e33ec1dde5cd8c096f23969f007
Daniel <danieleejohnson@gmail.com>
2024-06-12 12:49:28 -0400

    feat: rename main api endpoint

--- app/controllers/api_controller.py
+++ app/controllers/api_controller.py
@@ -3,12 +3,14 @@ from app.services import staff_service, api_service
 api_blueprint = Blueprint("api", __name__)
 
 @api_blueprint.route("/api/<category>/<action>", methods=["POST"])
-def staff_delete(category: str, action: str):
+def post(category: str, action: str):
     # csrf token?
     # automatically calls jsonify
     test = request.get_json()
     print(test)
+    print(category)
+    print(action)
 
 
-    response = api_service.delete_item(category, action, request)
+    response = api_service.handle_post(category, action, request)
     return response.to_dict()
