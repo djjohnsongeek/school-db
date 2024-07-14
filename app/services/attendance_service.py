fa8d4c482371701af9fc4704e6e6e2e863be5403
djjohnsongeek <danieleejohnson@gmail.com>
2024-07-14 06:39:20 -0400

    todo: add todo list for loading attendance

--- app/services/attendance_service.py
+++ app/services/attendance_service.py
@@ -22,5 +22,24 @@ def get_attendance_info(request: Request) -> ApiResultItem:
     class_id = request.args.get("class_id")
     date_str = request.args.get("date")
 
+    # convert date_str to date
+    # convert class_id to int
+    # valdiate tehse results
+
+    # fetch class
+    # fetch class's attendance records for the selected date (month)
+        # build a list of 'events' to summerize that month's attendance information
+            # Each event should count the number of Presents, Tardys, or Absents
+            # { title: "P:1", start: "2024-07-14" }
+            # { title: "T:2", start: "2024-07-14" }
+            # { title: "A:0", start: "2024-07-14" }
+    # fetch class's roster of students
+        # build a representation of the class rosters attendance for the selected date
+        # [
+        #   { student: student_obj, attendance_value: "P", attendance_id: 1 }
+            # include attendance_id in case the user decides to update a previously set attendance value
+            # this means an update or insert
+        # ]
+
 
     return ApiResultItem([], { "test": "testing the api endpoint", "date": date_str, "class_id": class_id })
