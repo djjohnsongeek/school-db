4e3794d334d1b752289a9b92a180e5fe40de7791
djjohnsongeek <danieleejohnson@gmail.com>
2024-07-20 06:02:20 -0400

    feat: get attendance buttons working on the front end

--- app/static/js/attendance/edit.js
+++ app/static/js/attendance/edit.js
@@ -1,7 +1,8 @@
 const attendancePage = {
     calendar: null,
     calendarElement: null,
-    formData: new FormData(),
+    payload: {},
+    selectedDateElement: null,
     init: function() {
         // Handle changes to the currently selected class
         document.getElementById("attendance-class-select").addEventListener("change", function() {
@@ -9,21 +10,39 @@ const attendancePage = {
             attendancePage.loadRosterAttendance();
         });
 
+        document.getElementById("student-attendance-saveBtn").addEventListener("click", attendancePage.saveAttendance);
+
         attendancePage.calendarElement = document.getElementById('calendar');
         attendancePage.calendar = new FullCalendar.Calendar(attendancePage.calendarElement, {
             initialView: 'dayGridMonth',
             dateClick: attendancePage.onDateClicked,
             datesSet: attendancePage.onDatesSet
         });
+        // TODO: need to check url attributes for date string
         attendancePage.selectDate(attendancePage.currentDateStr());
         attendancePage.loadRosterAttendance();
+
         // render() calls the datesSet callback
         attendancePage.calendar.render();
     },
     onDatesSet: function(info) {
+        // TODO BUG:
+        // when moving to a new month, if the selected date is not part of that month
+        // that months' events do not show up
+        // we need to load events based on the current month of the calendar. Not based on the selected date field.
         attendancePage.loadAttendanceEvents();
     },
     onDateClicked: function(info) {
+        // style selected cell
+        // TODO: selected stylign is lost when moving between months
+        if (attendancePage.selectedDateElement !== null)
+        {
+            attendancePage.selectedDateElement.classList.remove("selected-cell");
+        }
+        attendancePage.selectedDateElement = info.dayEl;
+        attendancePage.selectedDateElement.classList.add("selected-cell");
+
+
         attendancePage.selectDate(info.dateStr);
     },
     selectDate: function(dateStr) {
@@ -47,9 +66,47 @@ const attendancePage = {
             e.remove();
         }
     },
-    loadAttendanceEvents: function()
-    {
-        console.log("Fetching attendance evnets");
+    onAttendanceBtnClick: function(event) {
+        const attendanceBtn = event.currentTarget;
+        const attendanceBtns = attendanceBtn.parentElement.children;
+
+        // Clear all btn styles
+        for (let btn of attendanceBtns)
+        {
+            btn.classList.remove("is-selected", "is-success", "is-danger", "is-warning");
+        }
+
+        // Define btn's color class
+        let colorClass = "";
+        switch(attendanceBtn.dataset.payload)
+        {
+            case "P":
+                colorClass = "is-success";
+                break;
+            case "T":
+                colorClass = "is-warning";
+                break;
+            case "A":
+                colorClass = "is-danger";
+                break;
+            default:
+                throw new RangeError(`Invalid Attendance Payload Value: ${attendanceBtn.dataset.payload}`);
+        }
+
+        // Set clicked btn's style
+        attendanceBtn.classList.add("is-selected", colorClass);
+
+        // Set attendance data
+        const attendanceValue = attendanceBtn.dataset.payload;
+        const attendanceId = parseInt(attendanceBtn.dataset.attendanceId);
+        const studentId = parseInt(attendanceBtn.dataset.studentId);
+        attendancePage.payload[studentId] = { studentId, attendanceId, attendanceValue};
+    },
+    clearAttendancePayload: function() {
+        attendancePage.payload = {};
+    },
+    loadAttendanceEvents: function() {
+        console.log("Fetching attendance events");
         const classId = parseInt(document.getElementById("attendance-class-select").value);
         const date = document.getElementById("selected-date-input").value;
         requestObj = {
@@ -75,8 +132,7 @@ const attendancePage = {
         }
         AsyncApi.getRequest(requestObj);
     },
-    loadRosterAttendance: function()
-    {
+    loadRosterAttendance: function() {
         console.log("Fetching roster attendance");
         const classId = parseInt(document.getElementById("attendance-class-select").value);
         const date = document.getElementById("selected-date-input").value;
@@ -88,8 +144,6 @@ const attendancePage = {
                 date: date
             },
             successCallback: function(responseData) {
-                console.log(responseData.data);
-
                 // Show or hide no students message
                 const msgContainer = document.getElementById("student-attendance-messages-container");
                 const saveBtn = document.getElementById("student-attendance-saveBtn");
@@ -105,22 +159,18 @@ const attendancePage = {
                     saveBtn.classList.remove("is-hidden");
                 }
 
-                // Render student names
+                // Render student roster attendance elements
+                const studentAttendanceContainer = document.getElementById("student-attendance-container");
                 const studentInfoContainer = document.getElementById("student-info-container");
                 studentInfoContainer.replaceChildren();
-                for (const item of responseData.data.rosterAttendance)
-                {
-                    const studentSpan = Util.toHtml(`<div class='mb-1'><span class="button is-static">${item.student.name}</span></div>`);
-                    studentInfoContainer.appendChild(studentSpan);
-                }
-                
-                // Render student attedance
-                const studentAttendanceContainer = document.getElementById("student-attendance-container");
                 studentAttendanceContainer.replaceChildren();
                 for (const item of responseData.data.rosterAttendance)
                 {
-                    console.log(item);
-                    // is-selected is-success|is-danger|is-warning
+                    // render student names
+                    const studentSpan = Util.toHtml(`<div class='mb-1'><span class="button is-static">${item.student.name}</span></div>`);
+                    studentInfoContainer.appendChild(studentSpan);
+
+                    // render student buttons
                     const presentSelectedClasses = item.attendance_value == "P" ? " is-selected is-success" : "";
                     const tardySelectedClasses = item.attendance_value == "T" ? " is-selected is-warning" : "";
                     const absentSelectedClasses = item.attendance_value == "A" ? " is-selected is-danger" : "";
@@ -128,7 +178,12 @@ const attendancePage = {
                     const presentBtn = Util.toHtml(`<button class="button${presentSelectedClasses}" data-student-id="${item.student.id}" data-attendance-id="${item.attendance_id}" data-payload="P">Present</button>`);
                     const tardyBtn = Util.toHtml(`<button class="button${tardySelectedClasses}" data-student-id="${item.student.id}" data-attendance-id="${item.attendance_id}" data-payload="T">Tardy</button>`);
                     const absentBtn = Util.toHtml(`<button class="button${absentSelectedClasses}" data-student-id="${item.student.id}" data-attendance-id="${item.attendance_id}" data-payload="A">Absent</button>`);
-
+                    
+                    for (let btn of [presentBtn, tardyBtn, absentBtn])
+                    {
+                        btn.addEventListener("click", attendancePage.onAttendanceBtnClick);
+                    }
+                    
                     const buttonsContainer = Util.toHtml(`<div class="buttons has-addons mb-1"></div>`);
                     buttonsContainer.appendChild(presentBtn);
                     buttonsContainer.appendChild(tardyBtn);
@@ -136,12 +191,30 @@ const attendancePage = {
 
                     studentAttendanceContainer.appendChild(buttonsContainer);
                 }
+                attendancePage.clearAttendancePayload();
             },
             errorCallback: function() {
                 console.log("ERROR!");
             }
         }
         AsyncApi.getRequest(requestObj);
+    },
+    saveAttendance: function() {
+        const classId = parseInt(document.getElementById("attendance-class-select").value);
+        const date = document.getElementById("selected-date-input").value;
+        const attendance = [];
+        for (const value of Object.values(attendancePage.payload))
+        {
+            attendance.push(value);
+        }
+
+        const postData = {
+            classId,
+            date,
+            attendance
+        }
+
+        console.log(postData);
     }
 }
 
