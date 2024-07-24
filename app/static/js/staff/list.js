7ff1d76bc724bc409e21fd74a674c5f00a4a850e
djjohnsongeek <danieleejohnson@gmail.com>
2024-07-24 06:46:47 -0400

    feat: font end changes to support resetting a staff's password

--- app/static/js/staff/list.js
+++ app/static/js/staff/list.js
@@ -5,3 +5,31 @@ let deleteRequestConfig = {
 }
 
 document.getElementById("confirm-delete-btn").addEventListener("click", (event) => AppApi.deleteRequest(event, deleteRequestConfig));
+document.getElementById("").addEventListener("click", function(event) {
+    const modalElement = event.currentTarget.closest(".modal");
+    const staffId = parseInt(modalElement.dataset.itemId);
+    new_password = document.getElementById("pwreset-password").value;
+
+    let options = {
+        category: "password",
+        action: "reset",
+        data: {
+            staff_id: staffId,
+            password: new_password
+        },
+        successCallback: function(responseData) {
+            if (responseData.errors.length == 0)
+            {
+                Messages.addMessage("Password reset successfully!", "is-success");
+                // all is well
+            }
+
+            // error messages have already been displaye
+        },
+        errorCallback: function() {
+             // dont close modal
+        }
+    };
+
+    AsyncApi.postRequest(options);
+});
