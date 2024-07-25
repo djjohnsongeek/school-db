0703d9d6d9943d64c78daa50f23cf8074591d364
djjohnsongeek <danieleejohnson@gmail.com>
2024-07-24 20:37:46 -0400

    feat: add event listener to save reset btn

--- app/static/js/staff/list.js
+++ app/static/js/staff/list.js
@@ -5,7 +5,7 @@ let deleteRequestConfig = {
 }
 
 document.getElementById("confirm-delete-btn").addEventListener("click", (event) => AppApi.deleteRequest(event, deleteRequestConfig));
-document.getElementById("").addEventListener("click", function(event) {
+document.getElementById("pwreset-save-btn").addEventListener("click", function(event) {
     const modalElement = event.currentTarget.closest(".modal");
     const staffId = parseInt(modalElement.dataset.itemId);
     new_password = document.getElementById("pwreset-password").value;
@@ -20,11 +20,10 @@ document.getElementById("").addEventListener("click", function(event) {
         successCallback: function(responseData) {
             if (responseData.errors.length == 0)
             {
-                Messages.addMessage("Password reset successfully!", "is-success");
-                // all is well
+                Messages.addMessage("Password reset successfully!", "success");
+                document.getElementById("pwreset-password").value = "";
+                Modal.close(modalElement);
             }
-
-            // error messages have already been displaye
         },
         errorCallback: function() {
              // dont close modal
