ccedef493f8b83867c9a8a0cf198220e636fb8e9
djjohnsongeek <danieleejohnson@gmail.com>
2024-07-29 06:23:17 -0400

    feat: adjusting logging location

--- app/services/log_service.py
+++ app/services/log_service.py
@@ -1,8 +1,10 @@
 from flask import current_app
 from datetime import datetime
+import os
 
 def record_log(msg: str, app_area: str, log_type: str):
     log_path = current_app.config["LOG_FILE_PATH"]
+    log_path = os.path.join(current_app.root_path, log_path)
     f = open(log_path, "a")
     timestamp = datetime.now()
     log_str = f"{log_type.upper()}: {app_area} - {msg} ({timestamp})\n"
