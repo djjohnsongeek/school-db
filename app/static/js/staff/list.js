3bd58280b6bf07db411b7d0d59ba4da5c9dd7dd0
Daniel Johnson <danieleejohnson@gmail.com>
2025-05-20 02:30:09 -0400

    feat: reorganize js a bit

--- app/static/js/staff/list.js
+++ app/static/js/staff/list.js
@@ -1,43 +1,46 @@
-let deleteRequestConfig = {
-    category: "staff",
-    successMsg : "The staff member was successfully deleted.",
-    rowToRemoveIdPrefix: "staff-member-row-",
-}
-
-document.getElementById("confirm-delete-btn").addEventListener("click", (event) => AppApi.deleteRequest(event, deleteRequestConfig));
-document.getElementById("pwreset-save-btn").addEventListener("click", function(event) {
-    const modalElement = event.currentTarget.closest(".modal");
-    const staffId = parseInt(modalElement.dataset.itemId);
-    new_password = document.getElementById("pwreset-password").value;
-
-    let options = {
-        category: "password",
-        action: "reset",
-        data: {
-            staff_id: staffId,
-            password: new_password
-        },
-        successCallback: function(responseData) {
-            if (responseData.errors.length == 0)
-            {
-                Messages.addMessage("Password reset successfully!", "success");
-                document.getElementById("pwreset-password").value = "";
-                Modal.close(modalElement);
-            }
-        },
-        errorCallback: function() {
-             // dont close modal
-        }
-    };
-
-    AsyncApi.postRequest(options);
-});
-
 $(document).ready( function () {
+    // Setup datatable
     const table = $('#staff-table').DataTable();
+
     document.getElementsByClassName("dt-search")[0].style.display = "none";
         document.getElementById("staff-search-input").addEventListener("keyup", (event) => {
         const queryString = event.currentTarget.value;
         table.search(queryString).draw();
     });
-} );
+
+    // Setup table row buttons
+    let deleteRequestConfig = {
+        category: "staff",
+        successMsg : "The staff member was successfully deleted.",
+        rowToRemoveIdPrefix: "staff-member-row-",
+    }
+
+    document.getElementById("confirm-delete-btn").addEventListener("click", (event) => AppApi.deleteRequest(event, deleteRequestConfig));
+    document.getElementById("pwreset-save-btn").addEventListener("click", function(event) {
+        const modalElement = event.currentTarget.closest(".modal");
+        const staffId = parseInt(modalElement.dataset.itemId);
+        new_password = document.getElementById("pwreset-password").value;
+
+        let options = {
+            category: "password",
+            action: "reset",
+            data: {
+                staff_id: staffId,
+                password: new_password
+            },
+            successCallback: function(responseData) {
+                if (responseData.errors.length == 0)
+                {
+                    Messages.addMessage("Password reset successfully!", "success");
+                    document.getElementById("pwreset-password").value = "";
+                    Modal.close(modalElement);
+                }
+            },
+            errorCallback: function() {
+                // dont close modal
+            }
+        };
+
+        AsyncApi.postRequest(options);
+    });
+});
--- app/static/js/students/list.js
+++ app/static/js/students/list.js
@@ -1,16 +1,18 @@
-let deleteRequestConfig = {
-    category: "student",
-    successMsg : "The student was successfully deleted.",
-    rowToRemoveIdPrefix: "student-row-",
-}
-
-document.getElementById("confirm-delete-btn").addEventListener("click", (event) => AppApi.deleteRequest(event, deleteRequestConfig));
-
 $(document).ready( function () {
+    // Setup data table
     const table = $('#students-table').DataTable();
     document.getElementsByClassName("dt-search")[0].style.display = "none";
     document.getElementById("students-search-input").addEventListener("keyup", (event) => {
         const queryString = event.currentTarget.value;
         table.search(queryString).draw();
     });
-} );
+
+    // Setup table row buttons
+    let deleteRequestConfig = {
+        category: "student",
+        successMsg : "The student was successfully deleted.",
+        rowToRemoveIdPrefix: "student-row-",
+    }
+
+    document.getElementById("confirm-delete-btn").addEventListener("click", (event) => AppApi.deleteRequest(event, deleteRequestConfig));
+});
